e6df2afc7ffced4c037f79f1d17d5ebf7a7ca752
nonis3 <schwar12@purdue.edu>
2017-02-10 19:13:52 +0000

    Minimizing GenServer code

--- lib/phrampu.ex
+++ lib/phrampu.ex
@@ -11,16 +11,11 @@ defmodule Phrampu do
     {:ok, state}
   end
 
-  def push(pid, item) do
-    GenServer.cast(pid, {:push, item})
-  end
-
   def who(pid, hostname) do
     GenServer.call(pid, {:who, hostname})
   end
 
   # Server (callbacks)
-
   def handle_call({:who, hostname}, _from, state) do
     thing = WhoModule.getWho(hostname)
       |> WhoModule.getStructs
@@ -29,13 +24,5 @@ defmodule Phrampu do
 
   def handle_call(request, from, state) do
     super(request, from, state)
-  end
-
-  def handle_cast({:push, item}, state) do
-    {:noreply, [item | state]}
-  end
-
-  def handle_cast(request, state) do
-    super(request, state)
   end
 end
