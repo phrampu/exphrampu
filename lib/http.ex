1a431f05e6855ed683bfe21f2eed61427087b569
nonis3 <schwar12@purdue.edu>
2017-02-11 20:24:02 +0000

    Tabs -> Spaces in http.ex

--- lib/http.ex
+++ lib/http.ex
@@ -2,38 +2,38 @@ defmodule HTTPHandler do
   def init(request, state) do
     pidnum  = :cowboy_req.binding(:pidnum, request)
 
-		api_result = Phrampu.state(pidnum)
+    api_result = Phrampu.state(pidnum)
 
-		request = :cowboy_req.reply( 
-			200,
-			[ {"content-type", "application/json"} ],
-			Poison.encode!(api_result),
-			request
-		)
+    request = :cowboy_req.reply( 
+      200,
+      [ {"content-type", "application/json"} ],
+      Poison.encode!(api_result),
+      request
+    )
 
-		{:ok, request, state}
-	end
+    {:ok, request, state}
+  end
 
-	def terminate(reason, request, state) do
-		:ok
-	end
+  def terminate(reason, request, state) do
+    :ok
+  end
 end
 
 defmodule HTTPModule do
 
-	def start do
+  def start do
     routes = [
       {"/", :cowboy_static, {:priv_file, :phrampu, "index.html"}},
       {"/phrampu/:pidnum", HTTPHandler, []},
     ]
 
-		dispatch_config = :cowboy_router.compile([{ :_, routes}])
+    dispatch_config = :cowboy_router.compile([{ :_, routes}])
 
     opts = [{:port, 8080}]
 
     env = [dispatch: dispatch_config]
 
-		{:ok, _} = :cowboy.start_http(:http, 100, opts, [env: env])
-	end
+    {:ok, _} = :cowboy.start_http(:http, 100, opts, [env: env])
+  end
 end
 
