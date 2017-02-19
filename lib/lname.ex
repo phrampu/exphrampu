adf120aa7137e2ed4a9810fbcff6e66486895a91
nonis3 <schwar12@purdue.edu>
2017-02-19 23:11:59 +0000

    LName is accessible through a global name, and lookup returns nil or the element instead of an array

--- lib/lname.ex
+++ lib/lname.ex
@@ -7,6 +7,13 @@ defmodule LName do
   use GenServer
 
   # Client
+  def create() do
+    case GenServer.whereis({:global, LName}) do
+      nil -> start_link()
+      pid -> {:error, :lname_already_started, pid}
+    end
+  end
+
   def start_link() do
     table = :ets.new(:lname_lookup, [:ordered_set, :protected, :named_table])
 
@@ -20,20 +27,24 @@ defmodule LName do
       })
     end)
 
-    GenServer.start_link(__MODULE__, [table: table])
+    GenServer.start_link(__MODULE__, [table: table], name: {:global, LName})
   end
 
-  def state(pid, careerAcc) do
+  def lookup(pid, careerAcc) do
     GenServer.call(pid, {:lookup, careerAcc})
   end
 
   # Server (callbacks)
-  def init(:ok, {table}) do
+  def init(:ok, [table: _table]) do
     {:ok}
   end
 
   def handle_call({:lookup, careerAcc}, _from, [table: table]) do
     resp = :ets.lookup(table, careerAcc)
+    resp = case resp do
+      [x] -> x
+      [] -> nil
+    end
     {:reply, resp, [table: table]}
   end
 end
