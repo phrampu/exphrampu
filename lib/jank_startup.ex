11c7ce710dfb9f4f7d912b63828fc19c88dfcdce
nonis3 <schwar12@purdue.edu>
2017-02-19 20:37:20 +0000

    Adding sslab

--- lib/jank_startup.ex
+++ lib/jank_startup.ex
@@ -4,18 +4,23 @@ defmodule Jank do
   end
 
   def update do
-    Phrampu.Constants.podHosts 
-      |> Enum.each(fn(x) -> Phrampu.Server.who(ref("pod"), x) end)
+    spawn_link fn -> Phrampu.Constants.ssHosts 
+      |> Enum.each(fn(x) -> Phrampu.Server.who(ref("sslab"), x) end) end
 
-    Phrampu.Constants.borgHosts 
-      |> Enum.each(fn(x) -> Phrampu.Server.who(ref("borg"), x) end)
+    spawn_link fn -> Phrampu.Constants.podHosts 
+      |> Enum.each(fn(x) -> Phrampu.Server.who(ref("pod"), x) end) end
 
-    Phrampu.Constants.xinuHosts 
-      |> Enum.each(fn(x) -> Phrampu.Server.who(ref("xinu"), x) end)
+    spawn_link fn -> Phrampu.Constants.borgHosts 
+      |> Enum.each(fn(x) -> Phrampu.Server.who(ref("borg"), x) end) end
 
-    Phrampu.Constants.mooreHosts 
-      |> Enum.each(fn(x) -> Phrampu.Server.who(ref("moore"), x) end)
+    spawn_link fn -> Phrampu.Constants.xinuHosts 
+      |> Enum.each(fn(x) -> Phrampu.Server.who(ref("xinu"), x) end) end
 
+    spawn_link fn -> Phrampu.Constants.mooreHosts 
+      |> Enum.each(fn(x) -> Phrampu.Server.who(ref("moore"), x) end) end
+
+    spawn_link fn -> Phrampu.Constants.escherHosts 
+      |> Enum.each(fn(x) -> Phrampu.Server.who(ref("escher"), x) end) end
   end
 
   def go do
@@ -24,6 +29,8 @@ defmodule Jank do
     Phrampu.Server.create("borg", Phrampu.Constants.borgHosts)
     Phrampu.Server.create("pod", Phrampu.Constants.podHosts)
     Phrampu.Server.create("moore", Phrampu.Constants.mooreHosts)
+    Phrampu.Server.create("escher", Phrampu.Constants.escherHosts)
+    Phrampu.Server.create("sslab", Phrampu.Constants.ssHosts)
     spawn_link fn -> HTTPModule.start end
     spawn_link fn -> Jank.update() end
   end
