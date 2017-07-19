600b9672cdef1aa64f831d1add0cb739b02de782
nonis3 <nonis@purdue.edu>
2017-07-19 10:18:10 -0400

    Seeding file done

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,22 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+NimbleCSV.define(CSV, separator: ":", escape: ~s("))
+
+alias Phrampu.Student
+
+Phrampu.Repo.delete_all(Student)
+
+"lname.db"
+	|> File.stream!
+	|> CSV.parse_stream
+	|> Enum.map(fn [career_acc, name, email, machine | _] -> 
+		%{career_acc: career_acc, 
+			name: name |> String.split(",") |> List.first(), 
+			email: email, 
+			machine: machine} 
+		end)
+	 |> Enum.each(fn params ->
+		 changeset = Student.changeset(%Student{}, params)
+		 Phrampu.Repo.insert!(changeset)
+		end)
