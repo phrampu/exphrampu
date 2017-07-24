742856c211bc600122fd88e301f069b93cb9d1d1
nonis3 <nonis@purdue.edu>
2017-07-24 11:46:42 -0400

    Running Jank.go every 30 mins

--- lib/phrampu.ex
+++ lib/phrampu.ex
@@ -14,6 +14,7 @@ defmodule Phrampu do
       supervisor(Phrampu.Endpoint, []),
       # Start your own worker by calling: Phrampu.Worker.start_link(arg1, arg2, arg3)
       # worker(Phrampu.Worker, [arg1, arg2, arg3]),
+      worker(Phrampu.Periodically, [])
     ]
 
     # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
---
+++ lib/phrampu/periodically.ex
@@ -0,0 +1,21 @@
+defmodule Phrampu.Periodically do
+  use GenServer
+
+  def start_link do
+    GenServer.start_link(__MODULE__, %{})
+  end
+
+  def init(state) do
+    Jank.jank()
+    {:ok, state}
+  end
+
+  def handle_info(:work, state) do
+    Jank.jank()
+    {:noreply, state}
+  end
+
+  defp schedule_work() do
+    Process.send_after(self(), :work, 1 * 30 * 60 * 1000) # In 30 mins
+  end
+end
